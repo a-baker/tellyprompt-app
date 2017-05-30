21c4fb0cb566a4ef770c2388ac1fc22ad7fdc1b5
Aaron Baker <mail@abaker.co>
2017-05-30 13:46:51 +0100

    Ported existing code for MessageList, MessageForm components

--- src/components/chat/MessageForm.js
+++ src/components/chat/MessageForm.js
@@ -1,10 +1,62 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const MessageForm = () => {
-    return (
-        <div>
-        </div>
-    );
+class MessageForm extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            author: '',
+            text: ''
+        }
+    }
+
+    handleTextChange(e) {
+        let text = e.target.value;
+        this.setState({ text });
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+
+        let author = this.state.author.trim();
+        let content = this.state.text.trim();
+
+        if (!content) return;
+
+        let dateTime = new Date();
+        dateTime = dateTime.toISOString();
+
+        this.props.onMessageSubmit({
+            username,
+            content,
+            dateTime,
+            room
+        });
+
+        this.setState({
+            author: '',
+            text: ''
+        });
+    }
+
+    render() {
+        return (
+            <form
+                className="message-form"
+                onSubmit={this.handleSubmit}
+            >
+                <input
+                    placeholder="Say something..."
+                    value={this.state.text}
+                    onChange={this.handleTextChange}
+                />
+                <input
+                    type="submit"
+                    value="Post"
+                />
+            </form>
+        );
+    }
 }
 
 export default MessageForm;
--- src/components/chat/MessageList.js
+++ src/components/chat/MessageList.js
@@ -1,8 +1,37 @@
 import React from 'react';
 
-const MessageList = () => {
+function formatISOString(date){
+    var date = new Date(date);
+    var days = date.getDate();
+    days = (days < 10) ? ("0" + days) : days;
+    var months = date.getMonth() + 1;
+    months = (months < 10) ? ("0" + months) : months;
+    var hours = date.getHours();
+    hours = (hours < 10) ? ("0" + hours) : hours;
+    var minutes = date.getMinutes();
+    minutes = (minutes < 10) ? ("0" + minutes) : minutes;
+
+    var formattedDate =
+        days + "/" + months + "/" + date.getFullYear() + " at " + hours + ":" + minutes;
+    return formattedDate;
+}
+
+const MessageList = ({ messages }) => {
+    var messageNodes = messages.map(message => {
+        return (
+            <Message
+                author={message.username}
+                key={message.id}
+                dateTime={formatISOString(message.dateTime)}
+            >
+                {message.content}
+            </Message
+        );
+    });
+
     return (
-        <div>
+        <div className="message-list">
+            {messageNodes}
         </div>
     );
 }
